6f0735e9e5d229d1b1cb458f66646d25de8c48bb
Mathew <113514856+DooNeGo@users.noreply.github.com>
2022-11-23 11:05:01 +0300

    Add files via upload

---
+++ lab2/2/lab2.2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int n;
+	printf("Enter age: ");
+	scanf("%d",&n);
+	if((n>0) && (n<=13))
+		printf("Boy\n");
+	else if((n>=14) && (n<=20))
+		printf("Young man\n");
+	else if((n>=21) && (n<=70))
+		printf("Man\n");
+	else if(n>70)
+		printf("The elder\n");
+	system("pause");
+	return 0;
+} 
