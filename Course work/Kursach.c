83f5338c4517ba4ae7316aa4a17f82c7a9a936fc
Mathew <113514856+DooNeGo@users.noreply.github.com>
2022-12-09 18:53:11 +0300

    Add files via upload

--- Course work/Kursach.c
+++ Course work/Kursach.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-struct hospitalEmployee{
+struct hospitalEmployee
+{
     char surname[20];
     char name[20];
     char patronymic[20];
@@ -10,21 +11,25 @@ struct hospitalEmployee{
     int days;
     float paymentinOneDay;                                                                                                                                                        
 }a[50];
-struct users{
+struct users
+{
     char login[30];
     char password[30];
     int role;
 }a1[5];
-void array_nulling(char *array, int *size){
-    for (int i=0; i<*size; i++){
+void array_nulling(char *array, int *size)
+{
+    for (int i=0; i<*size; i++)
+    {
         *(array+i)=0;
     }
 }
-// Вход
-void pass(int *k, struct users *a1, int *size){
+void pass(int *k, struct users *a1, int *size)
+{
     int n, stop, maxNumofLetters=30;
     char login[maxNumofLetters], password[maxNumofLetters];
-    while (*k==0){
+    while (*k==0)
+    {
         array_nulling(login, &maxNumofLetters);
         array_nulling(password, &maxNumofLetters);
         stop=0;
@@ -32,9 +37,12 @@ void pass(int *k, struct users *a1, int *size){
         scanf("%s", &login);
         printf("Password: ");
         scanf("%s", &password);
-        for (int i=0; i<*size; i++){
-            if (memcmp((a1+i)->login, login, maxNumofLetters)==0 && memcmp((a1+i)->password, password, maxNumofLetters)==0){
-                if (a1[i].role==1){
+        for (int i=0; i<*size; i++)
+        {
+            if (memcmp((a1+i)->login, login, maxNumofLetters)==0 && memcmp((a1+i)->password, password, maxNumofLetters)==0)
+            {
+                if (a1[i].role==1)
+                {
                     *k=1;
                     printf("Enter succesful!\nYou have entered as admin\n");
                     break;
@@ -45,10 +53,12 @@ void pass(int *k, struct users *a1, int *size){
             }
         }
         if (*k==0) printf("\nWrong login or password\n");
-        while ((*k==0) && stop!=1){
+        while ((*k==0) && stop!=1)
+        {
             printf("1 - Try logging in again\n2 - Exit\n");
             scanf("%d", &n);
-            switch(n){
+            switch(n)
+            {
                 case 1:
                     stop=1;
                     break;
@@ -61,28 +71,36 @@ void pass(int *k, struct users *a1, int *size){
         }
     }
 }
-void outputInformationAboutHospitalEmployees(struct hospitalEmployee *a, int *size){
-    for (int i=0; i<*size; i++){
+void outputInformationAboutHospitalEmployees(struct hospitalEmployee *a, int *size)
+{
+    for (int i=0; i<*size; i++)
+    {
         printf("%d.|FIO: %2s %2s %2s | Year: %2d | Month: %2d | Number of days: %2d | Payment in one day: %2.3f|\n", i+1, (a+i)->surname, (a+i)->name, (a+i)->patronymic, (a+i)->years, (a+i)->months, (a+i)->days, (a+i)->paymentinOneDay);
     }
 }
-void editInformationAboutHospitalEmployees(struct hospitalEmployee *a, int *size){
+void editInformationAboutHospitalEmployees(struct hospitalEmployee *a, int *size)
+{
     int n, m=0, n1=0, m1=0;
-    while (m==0){
+    while (m==0)
+    {
         printf("Enter the employee's number(0-Exit): ");
         scanf("%d", &n1);
         m1=0;
-        if ((n1<0) || (n1>*size)){
+        if ((n1<0) || (n1>*size))
+        {
             printf("\nWrong number, please try again\n");
         }
-        else if (n1==0){
+        else if (n1==0)
+        {
                 m=1;     
         }
-        else while (m1==0){
+        else while (m1==0)
+        {
             printf("\n1 - Edit surname\n2 - Edit name\n3 - Edit patronymic\n4 - Edit Number of years of absence due to illness");
             printf("\n5 - Edit number of month of absence due to illness\n6 - Edit number of days of absence due to illness\n7 - Edit payment in one day\n0 - Exit\n");
                 scanf("%d", &n);
-                switch (n){
+                switch (n)
+                {
                     case 1:
                         printf("\nEnter new surname: ");
                         scanf("%s", &(a+n1-1)->surname);
@@ -122,7 +140,8 @@ void editInformationAboutHospitalEmployees(struct hospitalEmployee *a, int *size
         
     }
 }
-int main(){
+int main()
+{
     int k=0, size1, n, size2, m=0;
     struct users a1[]={"admin", "admin", 1, "user", "user", 0};
     size1=sizeof(a1)/sizeof(a1[0]);
@@ -130,11 +149,13 @@ int main(){
     size2=sizeof(a)/sizeof(a[0]);
     pass(&k, a1, &size1);
     system("pause");
-    if (k==2){
+    if (k==2)
+    {
         while (m==0){
             printf("1 - Display information about hospital employees\n2 - Exit\n");
             scanf("%d", &n);
-            switch(n){
+            switch(n)
+            {
                 case 1:
                     outputInformationAboutHospitalEmployees(a, &size2);
                     system("pause");
@@ -148,11 +169,14 @@ int main(){
             }
         }
     }
-    if (k==1){
-        while (m==0){
+    if (k==1)
+    {
+        while (m==0)
+        {
             printf("1 - Display information about hospital employees\n2 - Exit\n3 - Edit information about hospital employees\n");
             scanf("%d", &n);
-            switch(n){
+            switch(n)
+            {
                 case 1:
                     outputInformationAboutHospitalEmployees(a, &size2);
                     system("pause");
