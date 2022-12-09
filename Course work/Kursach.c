5996c01b782d10b76d368738a4b06743fdf4e91a
Mathew <113514856+DooNeGo@users.noreply.github.com>
2022-12-09 15:23:33 +0300

    Add files via upload

--- Course work/Kursach.c
+++ Course work/Kursach.c
@@ -11,31 +11,29 @@ struct hospitalEmployee{
 }a[50];
 
 struct users{
-    char login[15];
-    char password[15];
+    char login[30];
+    char password[30];
     int role;
 }a1[5];
-void array_nulling(char *array, int size){
-    for (int i=0; i<size; i++){
+void array_nulling(char *array, int *size){
+    for (int i=0; i<*size; i++){
         *(array+i)=0;
     }
 }
 // Вход
-void pass(int *k){
-    int n, stop, size;
-    char login[15], password[15];
-    struct users a1[]={"admin", "admin", 1, "user", "user", 0};
-    array_nulling(login, 15);
-    array_nulling(password, 15);
-    size=sizeof(a1)/sizeof(a1[0]);
+void pass(int *k, struct users *a1, int *size){
+    int n, stop, maxNumofLetters=30;
+    char login[maxNumofLetters], password[maxNumofLetters];
     while (*k==0){
+        array_nulling(login, &maxNumofLetters);
+        array_nulling(password, &maxNumofLetters);
         stop=0;
         printf("Login: ");
         scanf("%s", &login);
         printf("Password: ");
         scanf("%s", &password);
-        for (int i=0; i<size; i++){
-            if (memcmp(a1[i].login, login, 15)==0 && memcmp(a1[i].password, password, 15)==0){
+        for (int i=0; i<*size; i++){
+            if (memcmp((a1+i)->login, login, maxNumofLetters)==0 && memcmp((a1+i)->password, password, maxNumofLetters)==0){
                 if (a1[i].role==1){
                     *k=1;
                     printf("Enter succesful!\n");
@@ -50,7 +48,7 @@ void pass(int *k){
         while ((*k==0) && stop!=1){
             printf("1 - Try logging in again\n2 - Exit\n");
             scanf("%d", &n);
-            switch (n){
+            switch(n){
                 case 1:
                     stop=1;
                     break;
@@ -64,8 +62,10 @@ void pass(int *k){
     }
 }
 int main(){
-    int k=0;
-    pass(&k);
+    int k=0, size;
+    struct users a1[]={"admin", "admin", 1, "user", "user", 0};
+    size=sizeof(a1)/sizeof(a1[0]);
+    pass(&k, a1, &size);
     system("pause");
     return 0;
 }
