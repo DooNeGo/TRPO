09a6555497e87301879aad2be4d23d87f3f42282
Mathew <113514856+DooNeGo@users.noreply.github.com>
2022-12-13 20:56:51 +0300

    Big update
    New function of delete users and hospital employees

--- Course work/Kursach.c
+++ Course work/Kursach.c
@@ -24,12 +24,12 @@ void array_nulling(char *array, int *size)
         *(array+i)=0;
     }
 }
-void pass(int *k, struct users *a1, int *size, int *stopmain)
+void pass(int *role, struct users *a1, int *sizeUsers, int *stopmain, int *userNum)
 {
     int n, stop, maxNumofLetters=30;
     char login[maxNumofLetters], password[maxNumofLetters];
-    *k=0;
-    while (*k==0)
+    *role=0;
+    while (*role==0)
     {
         stop=0;
         array_nulling(login, &maxNumofLetters);
@@ -38,23 +38,25 @@ void pass(int *k, struct users *a1, int *size, int *stopmain)
         scanf("%s", &login);
         printf("Password: ");
         scanf("%s", &password);
-        for (int i=0; i<*size; i++)
+        for (int i=0; i<*sizeUsers; i++)
         {
-            if (memcmp((a1+i)->login, login, maxNumofLetters)==0 && memcmp((a1+i)->password, password, maxNumofLetters)==0)
+            if (strcmp((a1+i)->login, login)==0 && strcmp((a1+i)->password, password)==0)
             {
+                *userNum=i;
                 if ((a1+i)->role==1)
                 {
-                    *k=1;
+                    *role=1;
                     printf("Enter succesful! You have entered as an admin\n");
-                    break;
                 }
-                else *k=2;
-                printf("Enter succesful! You have entered as a user\n");
-                break;
+                else
+                {
+                    *role=2;
+                    printf("Enter succesful! You have entered as a user\n");
+                }
             }
         }
-        if (*k==0) printf("\nWrong login or password\n");
-        while ((*k==0) && stop!=1)
+        if (*role==0) printf("\nWrong login or password\n");
+        while ((*role==0) && stop!=1)
         {
             printf("1 - Try log in again\n2 - Close program\n");
             scanf("%d", &n);
@@ -64,7 +66,7 @@ void pass(int *k, struct users *a1, int *size, int *stopmain)
                     stop=1;
                     break;
                 case 2:
-                    *k=-1;
+                    *role=-1;
                     *stopmain=1;
                     break;
                 default:
@@ -72,25 +74,25 @@ void pass(int *k, struct users *a1, int *size, int *stopmain)
             }
         }
     }
-    if (*k!=-1) system("pause");
+    if (*role!=-1) system("pause");
 }
-void outputInformationAboutHospitalEmployees(struct hospitalEmployee *a, int *size)
+void outputHospitalEmployees(struct hospitalEmployee *a, int *sizeEmployee)
 {
-    for (int i=0; i<*size; i++)
+    for (int i=0; i<*sizeEmployee; i++)
     {
         printf("%d.|FIO: %2s %2s %2s | Year: %2d | Month: %2d | Number of days: %2d | Payment in one day: %2.3f|\n", i+1, (a+i)->surname, (a+i)->name, (a+i)->patronymic, (a+i)->years, (a+i)->months, (a+i)->days, (a+i)->paymentinOneDay);
     }
 }
-void editInformationAboutHospitalEmployees(struct hospitalEmployee *a, int *size)
+void editHospitalEmployees(struct hospitalEmployee *a, int *sizeEmployee)
 {
     int n, stop=0, n1, stop1=0;
     while (stop==0)
     {
-        outputInformationAboutHospitalEmployees(a, size);
+        outputHospitalEmployees(a, sizeEmployee);
         printf("Enter the employee's number(0-Exit): ");
         scanf("%d", &n1);
         stop1=0;
-        if ((n1<0) || (n1>*size)) printf("\nWrong number, please try again\n");
+        if ((n1<0) || (n1>*sizeEmployee)) printf("\nWrong number, please try again\n");
         else if (n1==0) stop=1;     
         else while (stop1==0)
         {
@@ -144,21 +146,82 @@ void editInformationAboutHospitalEmployees(struct hospitalEmployee *a, int *size
         }
     }
 }
-void addNewUser(struct users *a1, int *size)
+void deleteHospitalEmployee(struct hospitalEmployee *a, int *sizeEmployee)
 {
-    printf("Enter Login: ");
-    scanf("%s", &((a1+*size)->login));
-    printf("Enter Password: ");
-    scanf("%s", &((a1+*size)->password));
-    printf("Enter Role(1 or 0): ");
-    scanf("%d", &((a1+*size)->role));
-    printf("New user was added\n");
-    (*size)++;
+    int stop=0, n, i;
+    while (stop==0)
+    {
+        outputHospitalEmployees(a, sizeEmployee);
+        printf("Enter the employee's number(0-Exit): ");
+        scanf("%d", &n);
+        if ((n<0) || (n>*sizeEmployee)) printf("\nWrong number, please try again\n");
+        else if (n==0) stop=1;
+        else if (n==*sizeEmployee)
+        {
+            printf("Delete hospital employee successfully\n");
+            (*sizeEmployee)--;
+            system("pause");
+        }
+        else
+        {
+            for (i=n-1; i<*sizeEmployee-1; i++)
+            {
+                strcpy((a+i)->surname, (a+i+1)->surname);
+                strcpy((a+i)->name, (a+i+1)->name);
+                strcpy((a+i)->patronymic, (a+i+1)->patronymic);
+                (a+i)->years=(a+i+1)->years;
+                (a+i)->months=(a+i+1)->months;
+                (a+i)->days=(a+i+1)->days;
+                (a+i)->paymentinOneDay=(a+i+1)->paymentinOneDay;
+            }
+            (*sizeEmployee)--;
+            printf("Delete hospital employee successfully\n");
+            system("pause");
+        }
+    }
+}
+void addNewUser(struct users *a1, int *sizeUsers)
+{
+    int i, stop=0;
+    while (stop==0)
+    {
+        stop=1;
+        printf("Enter Login((Q)uit): ");
+        scanf("%s", &((a1+*sizeUsers)->login));
+        if (memcmp((a1+*sizeUsers)->login, "q", 1)==0 || memcmp((a1+*sizeUsers)->login, "Q", 1)==0) stop=2;
+        else for (i=0; i<*sizeUsers; i++)
+        {
+            if (strcmp((a1+*sizeUsers)->login, (a1+i)->login)==0)
+            {
+                printf("This login already exists\n");
+                stop=0;
+                break;
+            }
+        }
+    }
+    if (stop==1)
+    {
+        printf("Enter Password: ");
+        scanf("%s", &((a1+*sizeUsers)->password));
+        while (stop==1)
+        {
+            printf("Enter Role(1-admin or 2-user): ");
+            scanf("%d", &((a1+*sizeUsers)->role));
+            if ((a1+*sizeUsers)->role==1 || (a1+*sizeUsers)->role==2)
+            {
+                printf("New user was added\n");
+                (*sizeUsers)++;
+                stop=0;
+            }
+            else printf("\nWrong number of role, please try again\n");
+        }
+    }
     system("pause");
 }
-void outputUsers(struct users *a1, int *size)
+void outputUsers(struct users *a1, int *sizeUsers)
 {
-    for (int i=0; i<*size; i++) printf("%d.Login: %s  Password: %s  Role: %d\n", i+1, (a1+i)->login, (a1+i)->password, (a1+i)->role);
+    printf("\n");
+    for (int i=0; i<*sizeUsers; i++) printf("%d.Login: %s  Password: %s  Role: %d\n", i+1, (a1+i)->login, (a1+i)->password, (a1+i)->role);
 }
 void addNewHospitalEmployee(struct hospitalEmployee *a, int *size)
 {
@@ -176,7 +239,7 @@ void addNewHospitalEmployee(struct hospitalEmployee *a, int *size)
     (*size)++;
     system("pause");
 }
-void editUsers(struct users *a1, int *size)
+void editUsers(struct users *a1, int *size, int *userNum)
 {
     int stop=0, n, stop1=0, n1, stop2=0;
     while (stop==0)
@@ -185,7 +248,12 @@ void editUsers(struct users *a1, int *size)
         printf("Enter the user's number(0-Exit): ");
         scanf("%d", &n);
         if ((n<0) || (n>*size)) printf("\nWrong number, please try again\n");
-        else if (n==0) stop=1;     
+        else if (n==0) stop=1;
+        else if (n==(*userNum)+1)
+        {
+            printf("You can't edit an active user!!!\n");
+            system("pause");
+        }    
         else while (stop1==0)
         {
             printf("1 - Edit login\n2 - Edit password\n3 - Edit role\n0 - Exit\n");
@@ -205,9 +273,9 @@ void editUsers(struct users *a1, int *size)
                 case 3:
                     while (stop2==0)
                     {
-                        printf("Enter new role(1 or 0): ");
+                        printf("Enter new role(1-admin or 2-user): ");
                         scanf("%d", &(a1+n-1)->role);
-                        if ((a1+n-1)->role==1 || (a1+n-1)->role==0) stop2=1;
+                        if ((a1+n-1)->role==1 || (a1+n-1)->role==2) stop2=1;
                         else printf("\nWrong number, please try again\n");
                     }
                     printf("Role has been changed\n");
@@ -223,6 +291,41 @@ void editUsers(struct users *a1, int *size)
         }
     }
 }
+void deleteUser(struct users *a1, int *sizeUsers, int *userNum)
+{
+    int stop=0, n, i, k;
+    while (stop==0)
+    {
+        outputUsers(a1, sizeUsers);
+        printf("Enter the user's number(0-Exit): ");
+        scanf("%d", &n);
+        if ((n<0) || (n>*sizeUsers)) printf("\nWrong number, please try again\n");
+        else if (n==0) stop=1;
+        else if (n==(*userNum)+1)
+        {
+            printf("You can't delete an active user!!!\n");
+            system("pause");
+        }
+        else if (n==*sizeUsers)
+        {
+            printf("Delete user successfully\n");
+            (*sizeUsers)--;
+            system("pause");
+        }
+        else
+        {
+            for (i=n-1; i<*sizeUsers-1; i++)
+            {
+                strcpy((a1+i)->login, (a1+i+1)->login);
+                strcpy((a1+i)->password, (a1+i+1)->password);
+                (a1+i)->role=(a1+i+1)->role;
+            }
+            (*sizeUsers)--;
+            printf("Delete user successfully\n");
+            system("pause");
+        }
+    }
+}
 void usercapabilities(struct hospitalEmployee *a, int *sizeEmployee)
 {
     int stop=0, n;
@@ -233,7 +336,7 @@ void usercapabilities(struct hospitalEmployee *a, int *sizeEmployee)
         switch(n)
         {
             case 1:
-                outputInformationAboutHospitalEmployees(a, sizeEmployee);
+                outputHospitalEmployees(a, sizeEmployee);
                 system("pause");
                 break;
             case 0:
@@ -245,21 +348,21 @@ void usercapabilities(struct hospitalEmployee *a, int *sizeEmployee)
         }
     }     
 }
-void admincapabilities(struct hospitalEmployee *a, struct users *a1, int *sizeEmployee, int *sizeUsers)
+void admincapabilities(struct hospitalEmployee *a, struct users *a1, int *sizeEmployee, int *sizeUsers, int *userNum)
 {
     int stop=0, n;
     while (stop==0)
     {
-        printf("1 - Display information about hospital employees\n2 - Edit information about hospital employees\n3 - Add new user\n4 - Display all users\n5 - Add new hospital employee\n6 - Edit users\n0 - Log out\n");
+        printf("1 - Display information about hospital employees\n2 - Edit information about hospital employees\n3 - Add new user\n4 - Display all users\n5 - Add new hospital employee\n6 - Edit users\n7 - Delete user\n8 - Delete hospital employee\n0 - Log out\n");
         scanf("%d", &n);
         switch(n)
         {
             case 1:
-                outputInformationAboutHospitalEmployees(a, sizeEmployee);
+                outputHospitalEmployees(a, sizeEmployee);
                 system("pause");
                 break;
             case 2:
-                editInformationAboutHospitalEmployees(a, sizeEmployee);
+                editHospitalEmployees(a, sizeEmployee);
                 break;
             case 3:
                 addNewUser(a1, sizeUsers);
@@ -272,7 +375,13 @@ void admincapabilities(struct hospitalEmployee *a, struct users *a1, int *sizeEm
                 addNewHospitalEmployee(a, sizeEmployee);
                 break;
             case 6:
-                editUsers(a1, sizeUsers);
+                editUsers(a1, sizeUsers, userNum);
+                break;
+            case 7:
+                deleteUser(a1, sizeUsers, userNum);
+                break;
+            case 8:
+                deleteHospitalEmployee(a, sizeEmployee);
                 break;
             case 0:
                 stop=1;
@@ -285,25 +394,25 @@ void admincapabilities(struct hospitalEmployee *a, struct users *a1, int *sizeEm
 }
 int main()
 {
-    int k, sizeUsers=2, n, sizeEmployee=1, n1, stopmain=0;
-    struct users a1[5]={"admin", "admin", 1, "user", "user", 0};
+    int role, sizeUsers=3, n, sizeEmployee=1, n1, stopmain=0, userNum=-1;
+    struct users a1[5]={"admin", "admin", 1, "user", "user", 2, "test", "test", 2};
     struct hospitalEmployee a[10]={"Kostroma", "Matvey", "Olegovich", 2018, 11, 5, 8.54};
     while (stopmain==0)
     {
-        pass(&k, a1, &sizeUsers, &stopmain);
-        if (k==2) usercapabilities(a, &sizeEmployee);
-        if (k==1) admincapabilities(a, a1, &sizeEmployee, &sizeUsers);
-        while(k!=-1)
+        pass(&role, a1, &sizeUsers, &stopmain, &userNum);
+        if (role==2) usercapabilities(a, &sizeEmployee);
+        if (role==1) admincapabilities(a, a1, &sizeEmployee, &sizeUsers, &userNum);
+        while(role!=-1)
         {
             printf("1 - Log in\n0 - Close programm\n");
             scanf("%d", &n1);
             switch (n1)
             {
             case 1:
-                k=-1;
+                role=-1;
                 break;
             case 0:
-                k=-1;
+                role=-1;
                 stopmain=1;
                 break;
             default:
